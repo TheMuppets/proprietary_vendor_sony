5ddb6b884309551ad65f44497ffa70d523506070
Giulio Cervera <giulio.cervera@gmail.com>
2012-08-17 12:18:07 +0200

    nozomi: update blob list
    Change-Id: If30d90a8ac3d54f4b9750298abcf59839ad1d3bf

--- nozomi/nozomi-vendor-blobs.mk
+++ nozomi/nozomi-vendor-blobs.mk
@@ -18,8 +18,7 @@
 PRODUCT_COPY_FILES := \
     vendor/sony/nozomi/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
     vendor/sony/nozomi/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so \
-    vendor/sony/nozomi/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so \
-    vendor/sony/nozomi/proprietary/lib/libv8.so:obj/lib/libv8.so
+    vendor/sony/nozomi/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so
 
 PRODUCT_COPY_FILES += \
     vendor/sony/nozomi/proprietary/bin/bq275xx_fwloader:system/bin/bq275xx_fwloader \
@@ -170,14 +169,6 @@ PRODUCT_COPY_FILES += \
     vendor/sony/nozomi/proprietary/lib/libta_rmt_clnt.so:system/lib/libta_rmt_clnt.so \
     vendor/sony/nozomi/proprietary/lib/libthermal_mitigation.so:system/lib/libthermal_mitigation.so \
     vendor/sony/nozomi/proprietary/lib/libthermal_mitigation_fusion.so:system/lib/libthermal_mitigation_fusion.so \
-    vendor/sony/nozomi/proprietary/lib/libv8.so:system/lib/libv8.so \
-    vendor/sony/nozomi/proprietary/usr/idc/clearpad.idc:system/usr/idc/clearpad.idc \
-    vendor/sony/nozomi/proprietary/usr/keylayout/clearpad.kl:system/usr/keylayout/clearpad.kl \
-    vendor/sony/nozomi/proprietary/usr/keylayout/fuji-keypad.kl:system/usr/keylayout/fuji-keypad.kl \
-    vendor/sony/nozomi/proprietary/usr/keylayout/gpio-key.kl:system/usr/keylayout/gpio-key.kl \
-    vendor/sony/nozomi/proprietary/usr/keylayout/keypad-pmic-fuji.kl:system/usr/keylayout/keypad-pmic-fuji.kl \
-    vendor/sony/nozomi/proprietary/usr/keylayout/pmic8058_pwrkey.kl:system/usr/keylayout/pmic8058_pwrkey.kl \
-    vendor/sony/nozomi/proprietary/usr/keylayout/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
     vendor/sony/nozomi/proprietary/vendor/camera/APT01BM0.dat:system/vendor/camera/APT01BM0.dat \
     vendor/sony/nozomi/proprietary/vendor/camera/KMO13BS0.dat:system/vendor/camera/KMO13BS0.dat \
     vendor/sony/nozomi/proprietary/vendor/camera/KMO13BS0_BU6456.dat:system/vendor/camera/KMO13BS0_BU6456.dat \
