e5e00cc2238d0fda25b8b1e5a6013a123639f770
Alin Jerpelea <jerpelea@gmail.com>
2013-02-23 10:01:52 +0200

    qcom-common: blob cleanup

--- qcom-common/common-vendor-blobs.mk
+++ qcom-common/common-vendor-blobs.mk
@@ -25,12 +25,10 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/proprietary/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
     $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
     $(COMMON_PATH)/proprietary/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
     $(COMMON_PATH)/proprietary/system/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
     $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     $(COMMON_PATH)/proprietary/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     $(COMMON_PATH)/proprietary/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
     $(COMMON_PATH)/proprietary/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
     $(COMMON_PATH)/proprietary/system/lib/libC2D2.so:system/lib/libC2D2.so \
-    $(COMMON_PATH)/proprietary/system/lib/libOpenCL.so:system/lib/libOpenCL.so \
     $(COMMON_PATH)/proprietary/system/lib/libgsl.so:system/lib/libgsl.so
