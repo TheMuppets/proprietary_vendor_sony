221b97c273bd2309846e4ee1bc343e6d855b1df3
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-12-29 15:01:54 +0100

    qcom-common: updated adreno blob
    shipped with 14.2.A.0.290 (Sony 4.3)
    
    Change-Id: Idcf4d6b6a285b05e6f39952c51bfe03e1fde73b6

--- qcom-common/qcom-common-vendor-blobs-330.mk
+++ qcom-common/qcom-common-vendor-blobs-330.mk
@@ -29,14 +29,17 @@ PRODUCT_COPY_FILES += \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
-    vendor/sony/qcom-common/proprietary-330/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
-    vendor/sony/qcom-common/proprietary-330/vendor/lib/libc2d2_a3xx.so:system/vendor/lib/libc2d2_a3xx.so \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
-    vendor/sony/qcom-common/proprietary-330/vendor/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
-    vendor/sony/qcom-common/proprietary-330/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
-    vendor/sony/qcom-common/proprietary-330/vendor/lib/libllvm-a3xx.so:system/vendor/lib/libllvm-a3xx.so \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
+    vendor/sony/qcom-common/proprietary-330/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/sony/qcom-common/proprietary-330/vendor/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
+    vendor/sony/qcom-common/proprietary-330/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+    vendor/sony/qcom-common/proprietary-330/vendor/lib/libc2d30-a4xx.so:system/vendor/lib/libc2d30-a4xx.so \
+    vendor/sony/qcom-common/proprietary-330/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
+    vendor/sony/qcom-common/proprietary-330/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/sony/qcom-common/proprietary-330/vendor/lib/libllvm-a3xx.so:system/vendor/lib/libllvm-a3xx.so \
+    vendor/sony/qcom-common/proprietary-330/vendor/lib/libllvm-arm.so:system/vendor/lib/libllvm-arm.so \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
     vendor/sony/qcom-common/proprietary-330/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so
