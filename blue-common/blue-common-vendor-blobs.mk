13be460ba8ec364c2ba35d42b9817eb9a49358a6
Chirayu Desai <cdesai@cyanogenmod.org>
2014-06-10 22:34:53 +0530

    Adreno driver cleanup
    * Keep only the current driver in qcom-common and move
      the legacy / not latest versions to the platforms needing it.
    * Regenerate makefiles appropriately
    
    Change-Id: I7b70580638083840aaefb2aee2960218525eb3a5

--- blue-common/blue-common-vendor-blobs.mk
+++ blue-common/blue-common-vendor-blobs.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2013 The CyanogenMod Project
+# Copyright (C) 2011-2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,11 +16,36 @@
 
 PRODUCT_COPY_FILES += \
     vendor/sony/blue-common/proprietary/bin/system_monitor:system/bin/system_monitor \
+    vendor/sony/blue-common/proprietary/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+    vendor/sony/blue-common/proprietary/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
+    vendor/sony/blue-common/proprietary/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+    vendor/sony/blue-common/proprietary/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
+    vendor/sony/blue-common/proprietary/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
+    vendor/sony/blue-common/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    vendor/sony/blue-common/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
     vendor/sony/blue-common/proprietary/etc/firmware/wlan/prima/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     vendor/sony/blue-common/proprietary/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
-    vendor/sony/blue-common/proprietary/lib/libsysmon.so:system/lib/libsysmon.so \
+    vendor/sony/blue-common/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    vendor/sony/blue-common/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    vendor/sony/blue-common/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    vendor/sony/blue-common/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    vendor/sony/blue-common/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/sony/blue-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    vendor/sony/blue-common/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
+    vendor/sony/blue-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    vendor/sony/blue-common/proprietary/lib/libc2d2_a3xx.so:system/lib/libc2d2_a3xx.so \
+    vendor/sony/blue-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
+    vendor/sony/blue-common/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+    vendor/sony/blue-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
+    vendor/sony/blue-common/proprietary/lib/libllvm-a3xx.so:system/lib/libllvm-a3xx.so \
+    vendor/sony/blue-common/proprietary/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
+    vendor/sony/blue-common/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
+    vendor/sony/blue-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    vendor/sony/blue-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    vendor/sony/blue-common/proprietary/lib/libsc-a3xx.so:system/lib/libsc-a3xx.so \
     vendor/sony/blue-common/proprietary/lib/libsysmon_idd.so:system/lib/libsysmon_idd.so \
     vendor/sony/blue-common/proprietary/lib/libsysmon_jni.so:system/lib/libsysmon_jni.so \
+    vendor/sony/blue-common/proprietary/lib/libsysmon.so:system/lib/libsysmon.so \
     vendor/sony/blue-common/proprietary/lib/sysmon/sysmon_batt_therm.so:system/lib/sysmon/sysmon_batt_therm.so \
     vendor/sony/blue-common/proprietary/lib/sysmon/sysmon_camif.so:system/lib/sysmon/sysmon_camif.so \
     vendor/sony/blue-common/proprietary/lib/sysmon/sysmon_charge_current_limit_level.so:system/lib/sysmon/sysmon_charge_current_limit_level.so \
