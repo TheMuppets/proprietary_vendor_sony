f99b3cea99ba9a6fc58d5cab125f0789f2024044
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-06-14 19:15:28 +0200

    blob update
    drop hayabusa, no one maintain it
    drop msm8660 (aoba, hikari, nozomi)
    update mint from 9.2.A.1.205
    update tsubasa from 9.2.A.1.210
    enable supported sony feature on hushuan camera
    
    Change-Id: I1d132d0f708c37b04b2028d09f3cf154e6218d5e

--- aoba/proprietary/etc/clearpad_fwloader.sh
+++
@@ -1,2 +0,0 @@
-default_module_id=`/system/bin/ta_reader 1 2147543869 0`
-/system/bin/clearpad_fwloader default $default_module_id
--- hikari/proprietary/etc/clearpad_fwloader.sh
+++
@@ -1,2 +0,0 @@
-default_module_id=`/system/bin/ta_reader 1 60221 0`
-/system/bin/clearpad_fwloader default $default_module_id
