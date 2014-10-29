bd246dc2c8d0df7f5c4d389affb4366dd71ba151
Chirayu Desai <cdesai@cyanogenmod.org>
2014-10-29 23:26:01 +0530

    8226: regenerate makefiles
    * Regenerate makefiles by running the 'setup-makefiles.sh' script
      for tianchi, flamingo and msm8226-common
    Change-Id: I5d9797eb46c5714d0f57c341db65e2b7737d1cd3

--- msm8226-common/Android.mk
+++ msm8226-common/Android.mk
@@ -19,6 +19,20 @@ ifeq ($(BOARD_VENDOR_PLATFORM),yukon)
 
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(BOARD_USES_QC_TIME_SERVICES),true)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtime_genoff
+LOCAL_MODULE_OWNER := sony
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/vendor/lib
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
 LOCAL_MODULE_OWNER := sony
