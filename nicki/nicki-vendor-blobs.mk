40577dcf26fe0aa91d9dc26a391d40243b2dbaf3
RonGokhale <cips173@gmail.com>
2016-02-27 13:38:36 +0530

    nicki: update widevine blobs
     - widevine drm working now
    
    Change-Id: I0e94b13664beab2ad1e9b75f5106e998dbca9691

--- nicki/nicki-vendor-blobs.mk
+++ nicki/nicki-vendor-blobs.mk
@@ -178,4 +178,5 @@ PRODUCT_COPY_FILES += \
     vendor/sony/nicki/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
     vendor/sony/nicki/proprietary/vendor/lib/libulp2.so:system/vendor/lib/libulp2.so \
     vendor/sony/nicki/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-    vendor/sony/nicki/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so
+    vendor/sony/nicki/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/sony/nicki/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
