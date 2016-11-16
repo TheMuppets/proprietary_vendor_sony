d76c7f13e58862dada052efdf55f271ebb18b5b4
RonGokhale <cips173@gmail.com>
2016-11-16 11:58:20 +0530

    nicki: Build NFC firmware
     - Current nfc fw have text relocation.
     - Added buildable fw using Matt Mower's pnfw https://paste.ubuntu.com/18137835/
    
    Change-Id: I03c16e49e3fcb7ed2ed31c357d621c6cb8da77ae

--- nicki/nicki-vendor.mk
+++ nicki/nicki-vendor.mk
@@ -74,8 +74,7 @@ PRODUCT_COPY_FILES += \
     vendor/sony/nicki/proprietary/lib/libmorpho_scene_detector.so:system/lib/libmorpho_scene_detector.so \
     vendor/sony/nicki/proprietary/lib/libmorpho_super_resolution.so:system/lib/libmorpho_super_resolution.so \
     vendor/sony/nicki/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-    vendor/sony/nicki/proprietary/lib/libta.so:system/lib/libta.so \
-    vendor/sony/nicki/proprietary/vendor/firmware/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
+    vendor/sony/nicki/proprietary/lib/libta.so:system/lib/libta.so
 
 PRODUCT_PACKAGES += \
     tad_static
