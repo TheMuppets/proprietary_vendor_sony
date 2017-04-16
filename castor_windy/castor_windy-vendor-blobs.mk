c158a727930b51b3dfacb115bdbacf17da87f9aa
Max Weffers <rcstar6696@gmail.com>
2017-04-16 15:52:37 +0200

    castor_windy: Add Camera files from Stock for diffrent IMX134 from sony
    Change-Id: I505201e26743a5523ca3593327228f36df5589cd

--- castor_windy/castor_windy-vendor-blobs.mk
+++ castor_windy/castor_windy-vendor-blobs.mk
@@ -176,6 +176,56 @@ PRODUCT_COPY_FILES += \
 	vendor/sony/castor_windy/proprietary/vendor/camera/SOI08BS2/scene_detector.dat:system/vendor/camera/SOI08BS2/scene_detector.dat \
 	vendor/sony/castor_windy/proprietary/vendor/camera/SOI08BS2/snapshot_ctrl.dat:system/vendor/camera/SOI08BS2/snapshot_ctrl.dat \
 	vendor/sony/castor_windy/proprietary/vendor/camera/SOI08BS2/streaming_ctrl.dat:system/vendor/camera/SOI08BS2/streaming_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/cam_ctrl.dat:system/vendor/camera/SEM02BN1/cam_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/cap_ctrl.dat:system/vendor/camera/SEM02BN1/cap_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/chokoball.dat:system/vendor/camera/SEM02BN1/chokoball.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/color_ctrl.dat:system/vendor/camera/SEM02BN1/color_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/dataflow.dat:system/vendor/camera/SEM02BN1/dataflow.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/debug_mgr.dat:system/vendor/camera/SEM02BN1/debug_mgr.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/exposure_ctrl.dat:system/vendor/camera/SEM02BN1/exposure_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/face_detector.dat:system/vendor/camera/SEM02BN1/face_detector.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/flicker_detector.dat:system/vendor/camera/SEM02BN1/flicker_detector.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/focus_stats_conv.dat:system/vendor/camera/SEM02BN1/focus_stats_conv.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/hal_ctrl.dat:system/vendor/camera/SEM02BN1/hal_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/hal_ctrl_platform.dat:system/vendor/camera/SEM02BN1/hal_ctrl_platform.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/hdr_comp.dat:system/vendor/camera/SEM02BN1/hdr_comp.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/idt_ctrl.dat:system/vendor/camera/SEM02BN1/idt_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/image_conv.dat:system/vendor/camera/SEM02BN1/image_conv.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/imgproc.dat:system/vendor/camera/SEM02BN1/imgproc.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/jpeg_enc.dat:system/vendor/camera/SEM02BN1/jpeg_enc.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/lens_ctrl.dat:system/vendor/camera/SEM02BN1/lens_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/module.dat:system/vendor/camera/SEM02BN1/module.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/motion_estimation.dat:system/vendor/camera/SEM02BN1/motion_estimation.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/multiframe_nr.dat:system/vendor/camera/SEM02BN1/multiframe_nr.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/object_tracker.dat:system/vendor/camera/SEM02BN1/object_tracker.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/scene_detector.dat:system/vendor/camera/SEM02BN1/scene_detector.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/snapshot_ctrl.dat:system/vendor/camera/SEM02BN1/snapshot_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM02BN1/streaming_ctrl.dat:system/vendor/camera/SEM02BN1/streaming_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/cam_ctrl.dat:system/vendor/camera/SEM08BS2/cam_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/cap_ctrl.dat:system/vendor/camera/SEM08BS2/cap_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/chokoball.dat:system/vendor/camera/SEM08BS2/chokoball.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/color_ctrl.dat:system/vendor/camera/SEM08BS2/color_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/dataflow.dat:system/vendor/camera/SEM08BS2/dataflow.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/debug_mgr.dat:system/vendor/camera/SEM08BS2/debug_mgr.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/exposure_ctrl.dat:system/vendor/camera/SEM08BS2/exposure_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/face_detector.dat:system/vendor/camera/SEM08BS2/face_detector.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/flicker_detector.dat:system/vendor/camera/SEM08BS2/flicker_detector.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/focus_stats_conv.dat:system/vendor/camera/SEM08BS2/focus_stats_conv.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/hal_ctrl.dat:system/vendor/camera/SEM08BS2/hal_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/hal_ctrl_platform.dat:system/vendor/camera/SEM08BS2/hal_ctrl_platform.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/hdr_comp.dat:system/vendor/camera/SEM08BS2/hdr_comp.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/idt_ctrl.dat:system/vendor/camera/SEM08BS2/idt_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/image_conv.dat:system/vendor/camera/SEM08BS2/image_conv.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/imgproc.dat:system/vendor/camera/SEM08BS2/imgproc.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/jpeg_enc.dat:system/vendor/camera/SEM08BS2/jpeg_enc.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/lens_ctrl.dat:system/vendor/camera/SEM08BS2/lens_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/module.dat:system/vendor/camera/SEM08BS2/module.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/motion_estimation.dat:system/vendor/camera/SEM08BS2/motion_estimation.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/multiframe_nr.dat:system/vendor/camera/SEM08BS2/multiframe_nr.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/object_tracker.dat:system/vendor/camera/SEM08BS2/object_tracker.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/scene_detector.dat:system/vendor/camera/SEM08BS2/scene_detector.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/snapshot_ctrl.dat:system/vendor/camera/SEM08BS2/snapshot_ctrl.dat \
+	vendor/sony/castor_windy/proprietary/vendor/camera/SEM08BS2/streaming_ctrl.dat:system/vendor/camera/SEM08BS2/streaming_ctrl.dat \
 	vendor/sony/castor_windy/proprietary/vendor/camera/LGI02BN1/cam_ctrl.dat:system/vendor/camera/LGI02BN1/cam_ctrl.dat \
 	vendor/sony/castor_windy/proprietary/vendor/camera/LGI02BN1/cap_ctrl.dat:system/vendor/camera/LGI02BN1/cap_ctrl.dat \
 	vendor/sony/castor_windy/proprietary/vendor/camera/LGI02BN1/chokoball.dat:system/vendor/camera/LGI02BN1/chokoball.dat \
