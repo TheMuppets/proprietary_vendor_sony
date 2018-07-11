80c21c58440059e3e5a7292451dc206032f7b57c
Unmani <unmani@gmx.com>
2018-07-11 12:11:53 +0300

    add widevine

---
+++ widevine/widevine.mk
@@ -0,0 +1,5 @@
+
+PRODUCT_COPY_FILES += \
+	vendor/sony/widevine/proprietary/vendor/bin/hw/android.hardware.drm@1.0-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.0-service.widevine \
+	vendor/sony/widevine/proprietary/vendor/etc/init/android.hardware.drm@1.0-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.0-service.widevine.rc \
+	vendor/sony/widevine/proprietary/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so
