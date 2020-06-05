a962bffbfa72cc0fafa80d16e034c3e959b43604
LuK1337 <priv.luk@gmail.com>
2020-06-05 21:54:40 +0200

    discovery: Pull in libdevice_security_vendor.so

--- discovery/discovery-vendor.mk
+++ discovery/discovery-vendor.mk
@@ -275,6 +275,7 @@ PRODUCT_COPY_FILES += \
     vendor/sony/discovery/proprietary/vendor/lib/libchromatix_imx300_sm42_zsl_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_imx300_sm42_zsl_video.so \
     vendor/sony/discovery/proprietary/vendor/lib/libchromatix_imx300_sm42_zsl_video_hdr_3a.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_imx300_sm42_zsl_video_hdr_3a.so \
     vendor/sony/discovery/proprietary/vendor/lib/libdepthmapwrapper.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdepthmapwrapper.so \
+    vendor/sony/discovery/proprietary/vendor/lib/libdevice_security_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdevice_security_vendor.so \
     vendor/sony/discovery/proprietary/vendor/lib/libflash_pmic.so:$(TARGET_COPY_OUT_VENDOR)/lib/libflash_pmic.so \
     vendor/sony/discovery/proprietary/vendor/lib/libhal_dbg.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhal_dbg.so \
     vendor/sony/discovery/proprietary/vendor/lib/libjpegdhw.so:$(TARGET_COPY_OUT_VENDOR)/lib/libjpegdhw.so \
@@ -393,6 +394,7 @@ PRODUCT_COPY_FILES += \
     vendor/sony/discovery/proprietary/vendor/lib/libsuntory.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsuntory.so \
     vendor/sony/discovery/proprietary/vendor/lib/vendor.somc.hardware.security.secd@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.somc.hardware.security.secd@1.0.so \
     vendor/sony/discovery/proprietary/vendor/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so \
+    vendor/sony/discovery/proprietary/vendor/lib64/libdevice_security_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libdevice_security_vendor.so \
     vendor/sony/discovery/proprietary/vendor/lib64/libkeymasterdeviceutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymasterdeviceutils.so \
     vendor/sony/discovery/proprietary/vendor/lib64/libkeymasterprovision.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymasterprovision.so \
     vendor/sony/discovery/proprietary/vendor/lib64/libkeymasterutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymasterutils.so \
